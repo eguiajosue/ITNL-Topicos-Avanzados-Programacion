9ad2bb6c08bf9ef95c55c7fc8738f63a3b9ea95e
Josue Eguia <eguiajosue9@gmail.com>
2021-09-14 09:12:53 -0500

    Creación de la serie

--- Proyectos_Java/2.SerieFibonacci/src/App.java
+++ Proyectos_Java/2.SerieFibonacci/src/App.java
@@ -3,8 +3,15 @@ public class App {
         int valor1 = 0;
         int valor2 = 1;
 
+        int suma = 0;
+
         int ciclos = 10;
         
-        
+        for (int i = 0; i < ciclos; i++) {
+            System.out.println(valor1);
+            suma = valor1 + valor2;
+            valor1 = valor2;
+            valor2 = suma;    
+        }
     }
 }
